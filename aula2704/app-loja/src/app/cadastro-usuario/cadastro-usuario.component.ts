2f6322e85929aaad8057ecc45d4e49349c1c1402
Capgemini <jaqueline.costa@capemini.com>
2022-04-28 17:11:49 -0300

    feat: validações, logout e cadastro do usuario

--- aula2704/app-loja/src/app/cadastro-usuario/cadastro-usuario.component.ts
+++ aula2704/app-loja/src/app/cadastro-usuario/cadastro-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../usuario.service';
 
 @Component({
   selector: 'app-cadastro-usuario',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro-usuario.component.css']
 })
 export class CadastroUsuarioComponent implements OnInit {
+  usuarios: any;
+  constructor(private serviceUsuario: UsuarioService) {
+    this.serviceUsuario.getAll().subscribe(x => this.usuarios = x)
+   }
+  gravar(dados: any){
+    this.serviceUsuario.gravar(dados).subscribe(x => window.location.reload())
+  }
 
-  constructor() { }
+  excluir(id: any){
+    this.serviceUsuario.excluir(id).subscribe(x => {window.location.reload()})
+ }
+
 
   ngOnInit(): void {
   }

9ea5f613eece3c3d32d4b0c92f712957cd570af8
Capgemini <jaqueline.costa@capemini.com>
2022-05-02 11:55:06 -0300

    Rotas para CRUD de clientes, express

--- aula0205/index.js
+++ aula0205/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+
+var dados = require('./db')
 //esse middleware so vai aceitar o json 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
@@ -27,10 +29,73 @@ app.post('/contatos', (req,res) => {
     })
 })
 
+/**PEGAR Clientes */
+
+app.get('/clientes', (req,res) => {
+    res.status(201).send(dados)
+})
+
+//para consultar um cliente específico *****
+app.get('/clientes/:idclientes', (req, res) => {
+    let idclientes = req.params.idclientes;
+    let clienteretornar = {}
+    for(let i of dados){
+        if(i.id === idclientes){
+            clienteretornar = i
+        }
+    }
+    res.status(200).send(clienteretornar)
+});
+
+//para cadastrar novo cliente: inserir o objeto que mandei pela requisição*******
+app.post('/clientes', (req, res) => {
+    let obj = {
+        id: req.body.id,
+        nome: req.body.nome,
+        fone: req.body.fone,
+        limiteCredito: req.body.limiteCredito
+    }
+    dados.push(obj)
+    res.status(201).send(obj)
+})
+
+//para excluir clientes *******
+app.delete('/clientes/:idclientes', (req, res) => {
+    let idclientes = req.params.idclientes
+    let posicao = 0
+    for(let i of dados){
+        if(i.id == idclientes) {
+            break;
+        posicao++;
+        }
+    }
+    dados.splice(posicao, 1)
+    res.status(204).send();
+})
+
+
+//para alterar os dados dos clientes *******
+app.put('/clientes/:idclientes', (req, res) => {
+    let idclientes = req.params.idclientes
+    let clienteAlterado = {}
+    for(let i of dados){
+        if(i.id == idclientes){
+            i.nome = req.body.nome
+            i.fone = req.body.fone
+            i.limiteCredito = req.body.limiteCredito
+            clienteAlterado = i
+        }
+        
+    }
+    res.status(200).send(clienteAlterado)
+});
+
+
 app.delete('contatos/idcontato', (req, res) => {
     let idcontato = req.params.idcontato
     res.status(204).send();
 })
+
 
 //rota criada: dentro do recurso tem um objeto com um id
 app.post('/produtos', (req, res) => {

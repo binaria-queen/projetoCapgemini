f93f3d734d8e93de23392c69e1ebdff523b2de72
Capgemini <jaqueline.costa@capemini.com>
2022-05-02 16:54:02 -0300

    adicionar rotas para crud de produtos

---
+++ aula0205/produtos.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const app = express()
+var cors = require('cors')
+
+var dados = require('./dbprodutos')
+
+/**Middleware para utilizar urlencoded */
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+/**middleware cors*/
+app.use(cors());
+
+app.get('/produtos', (req, res) => {
+    res.status(200).send(dados)
+})
+
+app.post('/produtos', (req, res) => {
+    let ob = {
+        id: dados.length + 1,
+        descricao: req.body.descricao,
+        preco: req.body.preco,
+        estoque: req.body.estoque
+    }
+    dados.push(ob)
+    res.status(201).send(ob)
+})
+
+//para alterar os dados dos produtos *******
+app.put('/produtos/:idprodutos', (req, res) => {
+    let idprodutos = req.params.idprodutos
+    let produtoAlterado = {} 
+    for(let prod of dados){
+        if(prod.id == idprodutos){
+            prod.descricao = req.body.descricao
+            prod.preco = req.body.preco
+            prod.estoque = req.body.estoque
+            produtoAlterado = prod
+        }
+        
+    }
+    res.status(200).send(produtoAlterado)
+})
+
+app.get('/produtos/:idprodutos', (req, res) => {
+    let idprodutos = req.params.idprodutos
+    let produtoRetornado = {}
+    for(let prod of dados){
+        if(prod.id == idprodutos){
+            produtoRetornado = prod;
+            break;
+        }
+    }
+    res.status(200).send(produtoRetornado)
+})
+
+app.delete('/produtos/:idprodutos', (req, res) => {
+    let idprodutos = req.params.idprodutos
+    let posicao = 0
+    for(let prod of dados){
+        if(prod.id == idprodutos){
+            break;
+        }
+        posicao++
+    }
+    dados.splice(posicao, 1)
+    res.status(204).send()
+})
+
+app.listen(8081, () => console.log('Aplicação em execução na url http://localhost:8081'))
